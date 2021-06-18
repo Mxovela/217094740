11263770852c650025f4b322b4584ff628354430
MXO <MXO@DESKTOP-1TFNC6S>
2021-06-18 13:58:51 +0200

    commited

---
+++ Documents/Learning/Game Dev/LanguageLearningGame/LanguageLearningGame/QiuzGame1.0.0/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QiuzGame1._0._0
+{
+    public partial class Form1 : Form
+    {
+        int Score;
+        int Percentage;
+        int Questions = 1;
+        int Answer;
+        int Total;
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+
+            askQuestion(Questions);
+            Total = 7;
+        }
+        //Asking questions page
+        private void askQuestion(int QuestionNUmber)
+        {
+            switch (QuestionNUmber)
+            {
+                case 1:
+
+                    lblQuestion.Text = "It is used to travel long distance";
+
+                    button1.BackgroundImage = Properties.Resources.car;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Car";
+
+                    button2.BackgroundImage = Properties.Resources.human;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Human";
+
+                    button3.BackgroundImage = Properties.Resources.buikding;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Building";
+
+                    button4.BackgroundImage = Properties.Resources.tree;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "Tree";
+
+                    Answer = 1;
+
+                    break;
+                case 2:
+
+                    lblQuestion.Text = "Used to drink coffe";
+
+                    button1.BackgroundImage = Properties.Resources.Glass;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Glass";
+
+                    button2.BackgroundImage = Properties.Resources.cup;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Cup";
+
+                    button3.BackgroundImage = Properties.Resources.dish;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Dish";
+
+                    button4.BackgroundImage = Properties.Resources.spoon;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "spoon";
+
+                    Answer = 2;
+
+                    break;
+                case 3:
+
+                    lblQuestion.Text = "it is comfortable and used to sleep";
+
+
+                    button1.BackgroundImage = Properties.Resources.fridge;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Fridge";
+
+                    button2.BackgroundImage = Properties.Resources.closet;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Closet";
+
+                    button3.BackgroundImage = Properties.Resources.table;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Table";
+
+                    button4.BackgroundImage = Properties.Resources.bed;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "Bed";
+
+                    Answer = 4;
+
+                    break;
+                case 4:
+
+                    lblQuestion.Text = "when you are playing video games you use it to control game objects";
+
+                    button1.BackgroundImage = Properties.Resources.remote;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Remote";
+
+                    button2.BackgroundImage = Properties.Resources.radio;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Radio";
+
+                    button3.BackgroundImage = Properties.Resources.controller;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Controller";
+
+                    button4.BackgroundImage = Properties.Resources.TV;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "TV";
+
+                    Answer = 3;
+
+                    break;
+
+                case 5:
+
+                    lblQuestion.Text = "It is a device used to communocate with a person far away from you";
+
+
+                    button1.BackgroundImage = Properties.Resources.phone;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Phone";
+
+                    button2.BackgroundImage = Properties.Resources.charger;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "charger";
+
+                    button3.BackgroundImage = Properties.Resources.car;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "car";
+
+                    button4.BackgroundImage = Properties.Resources.fridge;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "Fridge";
+
+                    Answer = 1;
+
+                    break;
+
+                case 6:
+
+                    lblQuestion.Text = "You put in your clean clothes on";
+
+                    button1.BackgroundImage = Properties.Resources.bed;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Bed";
+
+                    button2.BackgroundImage = Properties.Resources.Glass;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Glass";
+
+                    button3.BackgroundImage = Properties.Resources.closet;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Closet";
+
+                    button4.BackgroundImage = Properties.Resources.TV;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "TV";
+
+                    Answer = 3;
+
+                    break;
+
+                case 7:
+
+                    lblQuestion.Text = "It is used to put food in mouth";
+
+                    button1.BackgroundImage = Properties.Resources.dish;
+                    button1.BackgroundImageLayout = ImageLayout.Stretch;
+                    button1.Text = "Dish";
+
+                    button2.BackgroundImage = Properties.Resources.couch;
+                    button2.BackgroundImageLayout = ImageLayout.Stretch;
+                    button2.Text = "Couch";
+
+                    button3.BackgroundImage = Properties.Resources.spoon;
+                    button3.BackgroundImageLayout = ImageLayout.Stretch;
+                    button3.Text = "Spoon";
+
+                    button4.BackgroundImage = Properties.Resources.fridge;
+                    button4.BackgroundImageLayout = ImageLayout.Stretch;
+                    button4.Text = "Fridge";
+
+                    Answer = 3;
+
+                    break;
+            }
+
+        }
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            Questions++;
+
+            askQuestion(Questions);
+            var senderObject = (Button)sender;
+
+            //converting a button tag from a string to integer
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == Answer) { Score++; }
+            if (Questions == Total)
+            {
+                //changing a percentage from double into integer
+                Percentage = (int)Math.Round((double)(Score * 100) / Total);
+
+                MessageBox.Show(
+
+                    "You Completed the Quiz!\n" +
+
+                    "Your got\t" + Score + " questions correctly.\n" +
+
+                    "You got \t" + Percentage + "%\n"
+                    );
+
+                Score = 0;
+
+                Questions = 0;
+
+                askQuestion(Questions);
+
+            }
+        }
+    }
+}
+
+namespace quizgame
+{
+    public class Form1
+    {
+        public Form1()
+        {
+        }
+
+        public DialogResult ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Documents/Learning/Game Dev/LanguageLearningGame/LanguageLearningGame/SplashScreen.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace LanguageLearningGame
+{
+    partial class frmSplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplashScreen));
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.BackColor = System.Drawing.Color.DimGray;
+            this.lblLoading.Font = new System.Drawing.Font("Candara", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.ForeColor = System.Drawing.Color.Coral;
+            this.lblLoading.Location = new System.Drawing.Point(183, 133);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(298, 78);
+            this.lblLoading.TabIndex = 0;
+            this.lblLoading.Text = "Loading...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(35, 272);
+            this.progressBar1.MarqueeAnimationSpeed = 50;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(570, 27);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = global::LanguageLearningGame.Properties.Resources.CrowImg;
+            this.pbLogo.Location = new System.Drawing.Point(12, 12);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(42, 36);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 2;
+            this.pbLogo.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(60, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Crow Learning";
+            // 
+            // frmSplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(637, 341);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblLoading);
+            this.Font = new System.Drawing.Font("Candara Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmSplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crow Learning";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label label2;
+    }
+}
